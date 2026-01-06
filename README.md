6e1682902bc9a94568c0cf283581aa5015b114fc
Sergey Karayev <sergeykarayev@gmail.com>
2026-01-05 16:36:10 -0800

    Initial

---
+++ README.md
@@ -0,0 +1,7 @@
+# X Host
+
+Export your X/Twitter data, and generate a single-page HTML archive of all of your tweets. Searchable, filterable, beautiful.
+
+## How to use
+
+TODO
---
+++ sample_export/data/tweets.js
@@ -0,0 +1,261 @@
+window.YTD.tweets.part0 = [
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ["1030618189682507777"],
+          editableUntil: "2018-08-18T01:51:39.037Z",
+          editsRemaining: "5",
+          isEditEligible: true,
+        },
+      },
+      retweeted: false,
+      source: '<a href="http://twitter.com" rel="nofollow">Twitter Web Client</a>',
+      entities: {
+        hashtags: [],
+        symbols: [],
+        user_mentions: [
+          {
+            name: "Tess",
+            screen_name: "TessFerrandez",
+            indices: ["12", "26"],
+            id_str: "25125547",
+            id: "25125547",
+          },
+        ],
+        urls: [
+          {
+            url: "https://t.co/gSgXgXD34l",
+            expanded_url: "http://deeplearning.ai",
+            display_url: "deeplearning.ai",
+            indices: ["40", "63"],
+          },
+          {
+            url: "https://t.co/jjfsShlA3t",
+            expanded_url:
+              "https://www.slideshare.net/TessFerrandez/notes-from-coursera-deep-learning-courses-by-andrew-ng",
+            display_url: "slideshare.net/TessFerrandez/…",
+            indices: ["72", "95"],
+          },
+        ],
+      },
+      display_text_range: ["0", "95"],
+      favorite_count: "16",
+      id_str: "1030618189682507777",
+      truncated: false,
+      retweet_count: "1",
+      id: "1030618189682507777",
+      possibly_sensitive: false,
+      created_at: "Sat Aug 18 00:51:39 +0000 2018",
+      favorited: false,
+      full_text: "Huge fan of @TessFerrandez notes on the https://t.co/gSgXgXD34l course: https://t.co/jjfsShlA3t",
+      lang: "en",
+    },
+  },
+  {
+    tweet: {
+      edit_info: {
+        initial: {
+          editTweetIds: ["1026269496686731265"],
+          editableUntil: "2018-08-06T01:51:29.832Z",
+          editsRemaining: "5",
+          isEditEligible: true,
+        },
+      },
+      retweeted: false,
+      source: '<a href="http://twitter.com/download/iphone" rel="nofollow">Twitter for iPhone</a>',
+      entities: {
+        user_mentions: [
+          {
+            name: "Yangqing Jia",
+            screen_name: "jiayq",
+            indices: ["22", "28"],
+            id_str: "32872803",
+            id: "32872803",
+          },
+          {
+            name: "Lukas Biewald",
+            screen_name: "l2k",
+            indices: ["33", "37"],
+            id_str: "13920962",
+            id: "13920962",
+          },
+          {
+            name: "Pieter Abbeel",
+            screen_name: "pabbeel",
+            indices: ["156", "164"],
+            id_str: "180993910",
+            id: "180993910",
+          },
+          {
+            name: "Josh Tobin",
+            screen_name: "josh_tobin_",
+            indices: ["166", "178"],
+            id_str: "331827860",
+            id: "331827860",
+          },
+          {
+            name: "Gradescope",
+            screen_name: "gradescope",
+            indices: ["184", "195"],
+            id_str: "2740674128",
+            id: "2740674128",
+          },
+        ],
+        urls: [],
+        symbols: [],
+        media: [
+          {
+            expanded_url: "https://x.com/sergeykarayev/status/1026269496686731265/photo/1",
+            indices: ["251", "274"],
+            url: "https://t.co/XklFcktwjE",
+            media_url: "http://pbs.twimg.com/media/Dj4KqZ4UUAAcLFJ.jpg",
+            id_str: "1026269488704933888",
+            id: "1026269488704933888",
+            media_url_https: "https://pbs.twimg.com/media/Dj4KqZ4UUAAcLFJ.jpg",
+            sizes: {
+              thumb: {
+                w: "150",
+                h: "150",
+                resize: "crop",
+              },
+              large: {
+                w: "2048",
+                h: "1536",
+                resize: "fit",
+              },
+              medium: {
+                w: "1200",
+                h: "900",
+                resize: "fit",
+              },
+              small: {
+                w: "680",
+                h: "510",
+                resize: "fit",
+              },
+            },
+            type: "photo",
+            display_url: "pic.x.com/XklFcktwjE",
+          },
+        ],
+        hashtags: [],
+      },
+      display_text_range: ["0", "274"],
+      favorite_count: "80",
+      id_str: "1026269496686731265",
+      truncated: false,
+      retweet_count: "28",
+      id: "1026269496686731265",
+      possibly_sensitive: false,
+      created_at: "Mon Aug 06 00:51:29 +0000 2018",
+      favorited: false,
+      full_text:
+        "Brilliant lectures by @jiayq and @l2k on the last day of the Full Stack Deep Learning bootcamp! It was an honor to host such a fantastic group of learners. @pabbeel, @josh_tobin_, the @gradescope crew and I are very thankful to everyone who attended! https://t.co/XklFcktwjE",
+      lang: "en",
+      extended_entities: {
+        media: [
+          {
+            expanded_url: "https://x.com/sergeykarayev/status/1026269496686731265/photo/1",
+            indices: ["251", "274"],
+            url: "https://t.co/XklFcktwjE",
+            media_url: "http://pbs.twimg.com/media/Dj4KqZ4UUAAcLFJ.jpg",
+            id_str: "1026269488704933888",
+            id: "1026269488704933888",
+            media_url_https: "https://pbs.twimg.com/media/Dj4KqZ4UUAAcLFJ.jpg",
+            sizes: {
+              thumb: {
+                w: "150",
+                h: "150",
+                resize: "crop",
+              },
+              large: {
+                w: "2048",
+                h: "1536",
+                resize: "fit",
+              },
+              medium: {
+                w: "1200",
+                h: "900",
+                resize: "fit",
+              },
+              small: {
+                w: "680",
+                h: "510",
+                resize: "fit",
+              },
+            },
+            type: "photo",
+            display_url: "pic.x.com/XklFcktwjE",
+          },
+          {
+            expanded_url: "https://x.com/sergeykarayev/status/1026269496686731265/photo/1",
+            indices: ["251", "274"],
+            url: "https://t.co/XklFcktwjE",
+            media_url: "http://pbs.twimg.com/media/Dj4KqZ4U8AANR-1.jpg",
+            id_str: "1026269488704974848",
+            id: "1026269488704974848",
+            media_url_https: "https://pbs.twimg.com/media/Dj4KqZ4U8AANR-1.jpg",
+            sizes: {
+              small: {
+                w: "680",
+                h: "510",
+                resize: "fit",
+              },
+              large: {
+                w: "2048",
+                h: "1536",
+                resize: "fit",
+              },
+              thumb: {
+                w: "150",
+                h: "150",
+                resize: "crop",
+              },
+              medium: {
+                w: "1200",
+                h: "900",
+                resize: "fit",
+              },
+            },
+            type: "photo",
+            display_url: "pic.x.com/XklFcktwjE",
+          },
+          {
+            expanded_url: "https://x.com/sergeykarayev/status/1026269496686731265/photo/1",
+            indices: ["251", "274"],
+            url: "https://t.co/XklFcktwjE",
+            media_url: "http://pbs.twimg.com/media/Dj4KqZ3UwAE3bge.jpg",
+            id_str: "1026269488700768257",
+            id: "1026269488700768257",
+            media_url_https: "https://pbs.twimg.com/media/Dj4KqZ3UwAE3bge.jpg",
+            sizes: {
+              large: {
+                w: "2048",
+                h: "1536",
+                resize: "fit",
+              },
+              medium: {
+                w: "1200",
+                h: "900",
+                resize: "fit",
+              },
+              thumb: {
+                w: "150",
+                h: "150",
+                resize: "crop",
+              },
+              small: {
+                w: "680",
+                h: "510",
+                resize: "fit",
+              },
+            },
+            type: "photo",
+            display_url: "pic.x.com/XklFcktwjE",
+          },
+        ],
+      },
+    },
+  },
+];
